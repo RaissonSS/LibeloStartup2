fb95490a4158932f2e8c833fbc43fc0e29e2beec
RaissonSS <raissonrai@gmail.com>
2022-11-06 20:04:02 -0300

    v0.1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("Teste")
+function login() {
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("password").value;
+
+  if (user == "")
+    alert("Nome de usuário vazio!");
+  else if (password == "")
+    alert("Senha de usuário vazia!");
+  else
+    window.location.assign("../pages/libeloPage.html");
+}
